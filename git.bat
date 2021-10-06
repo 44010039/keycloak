a48721ae0c9238074fe280d07b23184a60567bd0
PinWei.Wan <wanpinwei@outlook.com>
2021-10-06 10:53:50 +0800

    init

--- git.bat
+++ git.bat
@@ -1,2 +1,2 @@
-git config --global --replace-all user.email "yunjiaoyang@outlook.com" 
-git config --global --replace-all user.name "Lucky.Yang"
+git config --global --replace-all user.email "wanpinwei@outlook.com" 
+git config --global --replace-all user.name "PinWei.Wan"
